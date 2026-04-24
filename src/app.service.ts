934c4b68139982c7f7d0c697b43369aac0c27836
gberuashvili15G <g.beruashvili@noxtton.com>
2026-04-24 10:57:08 +0400

    feat: update current and minimum version numbers in AppService to 1.0.26

--- src/app.service.ts
+++ src/app.service.ts
@@ -12,11 +12,11 @@ export class AppService {
     forceUpdate: boolean;
   } {
     // currentVersion = ყველაზე ახალი ვერსია (უნდა ემთხვეოდეს app.json → version)
-    const currentVersion = '1.0.25';
+    const currentVersion = '1.0.26';
 
     // minVersion = ყველა ამ ვერსიაზე დაბალი დაააფდეითებს
     // მაგ: minVersion '1.0.24' → 1.0.23, 1.0.22... ხედავს Force Update მოდალს; 1.0.24+ არა
-    const minVersion = '1.0.25';
+    const minVersion = '1.0.26';
 
     const forceUpdate = true;
 
